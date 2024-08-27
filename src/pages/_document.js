a9e56d4124545621a800e013b61a2288040180cc
Changwoo Nam <whoshe@funnyplan.com>
2024-08-27 19:15:38 +0900

    노드 버전 관리 nvm 자동화 코드 추가

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,17 +3,20 @@ import Link from 'next/link'
 
 export default function Document() {
   return (
-    <Html lang="en">
-       <Head>
-       <Link rel="icon" href="/favicon.ico" />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-            <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet"></link>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
+    <Html lang='en'>
+      <Head>
+        <Link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        <link
+          href='https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap'
+          rel='stylesheet'
+        ></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
     </Html>
   )
 }
