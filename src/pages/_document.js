71f8fce8f4db0aa63af749bbb95dfbcdbbdc7771
whoshe <whoshe@gmail.com>
2024-08-28 13:03:23 +0900

    경고 UI 버그의 해결

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <Head>
         <Link rel='icon' href='/favicon.ico' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
         <link
           href='https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap'
           rel='stylesheet'
